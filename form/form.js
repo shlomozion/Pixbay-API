a21693903abf916667934ae4fc3144272c2cca57
shlomozion <jordan23zion@gmail.com>
2023-12-21 19:30:21 +0200

    pressing enter page refresh fix

--- form/form.js
+++ form/form.js
@@ -4,46 +4,20 @@ export const Form = () => {
   const results = document.getElementById("results");
 
   const form = document.createElement("form");
-  form.className = `
-  border 
-  flex
-  justify-center
-  items-center
-  gap-4
-  h-16
-  rounded
-  shadow-xl
-  
-  `;
+  form.className = `border flex justify-center items-center gap-4 h-16 rounded shadow-xl`;
 
   const input = document.createElement("input");
-  input.className = `
-  border 
-  border-slate-300
-  py-1
-  px-2
-  outline-none
-  rounded-xl
-  shadow-lg
-  focus:border-slate-500
-  `;
+  input.className = `border border-slate-300 py-1 px-2 outline-none rounded-xl shadow-lg focus:border-slate-500`;
   input.size = "30";
   input.type = "search";
 
   const button = document.createElement("button");
-  button.className = `
-  bg-slate-700
-  text-slate-200
-  p-2
-  rounded-lg
-  hover:text-slate-700
-  hover:bg-slate-300
-  font-bold
-  `;
+  button.className = `bg-slate-700 text-slate-200 p-2 rounded-lg hover:text-slate-700 hover:bg-slate-300 font-bold`;
   button.innerText = "Search";
-  button.type = "button";
+  button.type = "submit";
 
-  button.addEventListener("click", async () => {
+  button.addEventListener("click", async (e) => {
+    e.preventDefault();
     try {
       results.innerHTML = "";
       await PixbayAPI(input.value, "");
