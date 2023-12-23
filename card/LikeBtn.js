dd216f4046c745afc1132276e0ce4ff99d47be7f
shlomozion <jordan23zion@gmail.com>
2023-12-23 18:27:40 +0200

    added GetFavorites to unlikeBtn

--- card/LikeBtn.js
+++ card/LikeBtn.js
@@ -1,4 +1,3 @@
-import { GetFavorites } from "../actions/GetFavorites.js";
 import { SaveToFavorite } from "../actions/SaveToFavorite.js";
 
 export const LikeBtn = (id) => {
@@ -8,7 +7,6 @@ export const LikeBtn = (id) => {
   const favoriteBtn = document.createElement("div");
   favoriteBtn.addEventListener("click", () => {
     SaveToFavorite(id);
-    // GetFavorites();
   });
   favoriteBtn.innerHTML = "&#9829;";
   favoriteBtn.className = ` hover:cursor-pointer hover:bg-white hover:text-red-500 bg-red-500  px-3 max-sm:py-2 py-1	text-4xl text-white shadow-md rounded-full font-bold flex text-center`;
