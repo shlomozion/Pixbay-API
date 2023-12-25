c5dc068fc5bc15b76dab6b0343e09adbfaa9531f
shlomozion <jordan23zion@gmail.com>
2023-12-25 09:38:37 +0200

    link to Pixabay in modal fix

--- modal/LinkToPixbaySite.js
+++ modal/LinkToPixbaySite.js
@@ -1,11 +1,12 @@
 export const LinkToPixbaySite = (pageURL) => {
   const link = document.createElement("a");
+  link.className =
+    "bg-slate-700 text-slate-200 mx-2  py-2 px-3 rounded-lg hover:text-slate-700 hover:bg-slate-300 font-bold whitespace-nowrap";
   link.href = pageURL;
   link.target = "_blank";
-  const btn = document.createElement("button");
-  btn.className =
-    "bg-slate-700 text-slate-200 mx-2  py-2 px-3 rounded-lg hover:text-slate-700 hover:bg-slate-300 font-bold whitespace-nowrap";
-  btn.innerHTML = "Go to Pixbay site";
-  btn.appendChild(link);
-  return btn;
+  link.innerHTML = "Go to Pixbay site";
+  const container = document.createElement("div");
+  container.className = " flex justify-center";
+  container.appendChild(link);
+  return container;
 };
